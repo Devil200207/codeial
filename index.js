57fcf4adbfd35c982a32e1653c252724d4f72452
Devil200207 <74138871+Devil200207@users.noreply.github.com>
2022-08-18 19:21:57 +0530

    added ejs as a view engine and seted vides folder to see the views

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ const port = 8000;
 // use express router
 app.use('/',require('./routes'));
 
+// set ejs as view engine
+app.set('view engine','ejs');
+
+// set views folder for view
+app.set('views','./views');
 
 
 app.listen(port,function(err)
