080f3461aa37c9c57036736edceddc78cb7d6a19
Devil200207 <74138871+Devil200207@users.noreply.github.com>
2022-08-23 13:47:16 +0530

    added static file and used them with layouts

--- index.js
+++ index.js
@@ -5,11 +5,18 @@ const app = express();
 // using port
 const port = 8000;
 
+// setting up static file
+app.use(express.static('./assets'));
+
 // using express-ejs-layouts library
 const expressLayouts = require('express-ejs-layouts');
 
 // telling app that site which are called are layouts types
 app.use(expressLayouts);
+
+// extract style and script from sub pages to layout
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
 
 // use express router
 app.use('/',require('./routes'));
