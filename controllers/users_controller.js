a8f48dc2ff8fccb0f163204d5b3842b945a24ebf
Devil200207 <74138871+Devil200207@users.noreply.github.com>
2022-08-24 11:19:11 +0530

    added sign-in and sign-up page through routs,controllers and views

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -2,5 +2,20 @@
   {
     return res.render('users_profile',{
       title: "profile"
-  });
+    });
+  }
+
+  // render the sign-in and sign-up page
+  module.exports.signin = function(req,res)
+  {
+    return res.render('signin',{
+      title:"Codeial | Sign In"
+    });
+  }
+
+  module.exports.signup = function(req,res)
+  {
+    return res.render('signup',{
+      title:"Codeial | Sign Up"
+    });
   }
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ const router = express.Router();
 
 const userscontroller = require('../controllers/users_controller');
 const helpcontoller = require('../controllers/help_controllers');
+// const singincontroller = require('../controllers/users_controller');
+
+router.get('/signin',userscontroller.signin);
+router.get('/signup',userscontroller.signup);
 router.get('/profile',userscontroller.profile);
 router.get('/help',helpcontoller.help);
 
-module.exports = router;
+module.exports = router; 
