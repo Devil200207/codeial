ac4ae2ee762b8f059ddcc3ba216bc1e50e174b02
Devil200207 <74138871+Devil200207@users.noreply.github.com>
2022-09-05 10:19:37 +0530

    set an authentication to the post so that any user who is not sign-in cannot post anythink

--- routes/posts.js
+++ routes/posts.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const { Passport } = require('passport/lib');
 const router = express.Router();
+const passport = require('passport')
 
 const postcontroller = require('../controllers/posts_controllers');
 
-router.post('/create',postcontroller.create);
+router.post('/create',passport.checkAuthentication,postcontroller.create);
 
 module.exports = router;
